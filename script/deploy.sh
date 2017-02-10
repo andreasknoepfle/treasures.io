6e2844aeec4797a8cef2231734f1332b4ec05ee5
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2017-02-10 19:08:43 +0100

    automatic deployment

---
+++ script/deploy.sh
@@ -0,0 +1,7 @@
+echo $DEPLOY_KEY > /tmp/deploy_key.enc
+base64 --decode /tmp/deploy_key.enc > /tmp/deploy_key
+eval "$(ssh-agent -s)"
+chmod 600 /tmp/deploy_key
+ssh-add /tmp/deploy_key
+git remote add dokku dokku@treasures.io
+git push dokku master
