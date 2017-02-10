004c12901d23dfb55dfbc79fadc183e8d311e214
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2017-02-10 20:20:04 +0100

    try in script block for better output

--- script/deploy.sh
+++
@@ -1,7 +0,0 @@
-echo $DEPLOY_KEY > /tmp/deploy_key.enc
-base64 --decode /tmp/deploy_key.enc > /tmp/deploy_key
-eval "$(ssh-agent -s)"
-chmod 600 /tmp/deploy_key
-ssh-add /tmp/deploy_key
-git remote add dokku dokku@smallbutton.com:treasures
-git push -f dokku master
