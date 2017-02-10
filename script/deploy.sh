53b39ba5a9c15eee39dc15a69d17342610a49873
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2017-02-10 19:51:04 +0100

    changed creditals

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,5 +3,5 @@ base64 --decode /tmp/deploy_key.enc > /tmp/deploy_key
 eval "$(ssh-agent -s)"
 chmod 600 /tmp/deploy_key
 ssh-add /tmp/deploy_key
-git remote add dokku dokku@treasures.io
-git push dokku master
+git remote add dokku dokku@smallbutton.com:treasures
+git push -f dokku master
