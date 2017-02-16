6430d48f907617732a5255c293fbd351484f0361
joerg-seitz <joerg.seitz0@gmail.com>
2017-02-16 14:49:29 +0100

    api for oceans #38

--- spec/controllers/api/oceans_controller_spec.rb
+++ spec/controllers/api/oceans_controller_spec.rb
@@ -5,11 +5,13 @@ RSpec.describe Api::OceansController do
   let(:oceans) { json_response }
   let(:ocean_service) { instance_double(OceanService, call: 'ocean') }
 
+
   it 'renders 4 oceans, when there are no additional parameters' do
     get :index
     expect(response).to be_success
     expect(oceans.size).to be 4
   end
+
 
   it 'renders 0 oceans' do
     get :index, limit: '0'
@@ -27,5 +29,12 @@ RSpec.describe Api::OceansController do
   it 'calls the ocean with forbidden parameters (< 0 | > 20)' do
     get :index, limit: '21'
     expect(response).to be_bad_request
+  end
+
+  it 'calls the OceanService' do
+    expect(OceanService)
+      .to(receive(:new).exactly(4).times.and_return(ocean_service))
+    get :index
+    expect(oceans).to eq %w(ocean ocean ocean ocean)
   end
 end
