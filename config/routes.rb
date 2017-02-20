e310524eb8c085bee539d9640d5850e94bbf748f
joerg-seitz <joerg.seitz0@gmail.com>
2017-02-20 15:58:37 +0100

    added ressources to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'islands#index'
+  resources :islands, only: :index
   resources :test_oceans, only: :index
   namespace :api, defaults: { format: 'json' } do
     resources :oceans, only: :index
