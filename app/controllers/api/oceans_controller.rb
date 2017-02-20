17017493a7a17b6af16b1547b9975ee555930b60
joerg-seitz <joerg.seitz0@gmail.com>
2017-02-20 15:52:19 +0100

    added query-parameter

--- app/controllers/api/oceans_controller.rb
+++ app/controllers/api/oceans_controller.rb
@@ -1,6 +1,6 @@
 class Api::OceansController < ApplicationController
-  def index
-    oceans = Array.new(4) do
+  def index(n_oceans = 4)
+    oceans = Array.new(n_oceans) do
       OceanService.new(Random.rand(1..5)).call
     end
     render json: oceans
