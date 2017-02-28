cdc97b0e14fa02326ca795e05b04337fd9f3ba1b
joerg-seitz <joerg.seitz0@gmail.com>
2017-02-28 11:47:05 +0100

    #50 added query parameter

--- app/controllers/api/oceans_controller.rb
+++ app/controllers/api/oceans_controller.rb
@@ -1,6 +1,9 @@
 class Api::OceansController < ApplicationController
-  def index(n_oceans = 4)
-    oceans = Array.new(n_oceans) do
+  def index
+    params = Rack::Utils.parse_query URI(request.url).query
+    n_oceans = params["n_oceans"]
+    n_oceans = "1" if n_oceans.empty?
+    oceans = Array.new(n_oceans.to_i) do
       OceanService.new(Random.rand(1..5)).call
     end
     render json: oceans
