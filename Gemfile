a3486fbb06ac1f6d880fcbc101871fe82c262261
joergseitz <joerg.seitz0@gmail.com>
2017-03-01 18:39:35 +0100

    Json api (#51)
    * api for oceans #38
    
    * added query-parameter
    
    * added ressources to routes.rb
    
    * #50 added query parameter
    
    * #50 finished queryparameter and related  specs
    
    * #50 #51 finished json-API added parameter validations (<0 | >20)
    
    * minor fixes to make codeclimate happy
    
    * #5 cleaning up API-controller adding specs and cleaning up the Gemfile
    
    * #51 added the before filter...
    
    * finishing touches

--- Gemfile
+++ Gemfile
@@ -2,19 +2,16 @@ source 'https://rubygems.org'
 
 gem 'better_errors'
 gem 'binding_of_caller'
-gem 'byebug'
-
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
-# Use Puma as the app server
-gem 'puma', '~> 3.0'
-
-gem 'npm-pipeline-rails'
-gem 'slim'
-gem 'sass-rails'
 gem 'bourbon'
+gem 'byebug'
 gem 'neat'
 gem 'normalize-rails'
+gem 'npm-pipeline-rails'
+gem 'puma', '~> 3.0'
+gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
+gem 'sass-rails'
+gem 'slim'
+gem 'twitter'
 
 group :test do
   gem 'codecov', require: false
@@ -23,6 +20,7 @@ end
 group :development, :test do
   gem 'pry'
   gem 'pry-rails'
+  gem 'rspec-rails'
 end
 
 group :development do
@@ -33,4 +31,3 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'web-console'
 end
-gem 'rspec-rails', group: [:development, :test]
---
+++ spec/controllers/api/oceans_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Api::OceansController do
+  let(:json_response) { JSON.parse(response.body) }
+  let(:oceans) { json_response }
+  let(:ocean_service) { instance_double(OceanService, call: 'ocean') }
+
+  it 'renders 4 oceans, when there are no additional parameters' do
+    get :index
+    expect(response).to be_success
+    expect(oceans.size).to be 4
+  end
+
+  it 'renders 0 oceans' do
+    get :index, limit: '0'
+    expect(response).to be_success
+    expect(oceans.size).to be
+  end
+
+  it 'calls the OceanService with 4 as limit' do
+    expect(OceanService)
+      .to(receive(:new).exactly(4).times.and_return(ocean_service))
+    get :index, limit: '4'
+    expect(oceans).to eq %w(ocean ocean ocean ocean)
+  end
+
+  it 'calls the ocean with forbidden parameters (< 0 | > 20)' do
+    get :index, limit: '21'
+    expect(response).to be_bad_request
+  end
+end
