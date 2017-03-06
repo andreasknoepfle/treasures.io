001c560166f819c1f17ffbd8acfd109febe8ede8
joerg-seitz <joerg.seitz0@gmail.com>
2017-03-06 09:14:26 +0100

    added development.yml to gitignore -for twitter credentials
    integrated twitter gem, added development.yaml to .gitignore

--- app/controllers/api/oceans_controller.rb
+++ app/controllers/api/oceans_controller.rb
@@ -1,8 +1,25 @@
 class Api::OceansController < ApplicationController
+  protect_from_forgery with: :null_session
+  before_filter :render_bad_request, unless: :limit_valid?
+
   def index
-    oceans = Array.new(4) do
+    oceans = Array.new(number_of_oceans) do
       OceanService.new(Random.rand(1..5)).call
     end
     render json: oceans
+  end
+
+  private
+
+  def number_of_oceans
+    @number_of_oceans ||= params.fetch(:limit, 4).to_i
+  end
+
+  def render_bad_request
+    render nothing: true, status: :bad_request
+  end
+
+  def limit_valid?
+    number_of_oceans.between?(0, 20)
   end
 end
--- spec/controllers/api/oceans_controller_spec.rb
+++ spec/controllers/api/oceans_controller_spec.rb
@@ -5,16 +5,27 @@ RSpec.describe Api::OceansController do
   let(:oceans) { json_response }
   let(:ocean_service) { instance_double(OceanService, call: 'ocean') }
 
-  it 'renders 4 oceans' do
+  it 'renders 4 oceans, when there are no additional parameters' do
     get :index
     expect(response).to be_success
     expect(oceans.size).to be 4
   end
 
-  it 'calls the OceanService' do
+  it 'renders 0 oceans' do
+    get :index, limit: '0'
+    expect(response).to be_success
+    expect(oceans.size).to be
+  end
+
+  it 'calls the OceanService with 4 as limit' do
     expect(OceanService)
       .to(receive(:new).exactly(4).times.and_return(ocean_service))
-    get :index
+    get :index, limit: '4'
     expect(oceans).to eq %w(ocean ocean ocean ocean)
+  end
+
+  it 'calls the ocean with forbidden parameters (< 0 | > 20)' do
+    get :index, limit: '21'
+    expect(response).to be_bad_request
   end
 end
